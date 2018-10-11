907ca484bdfcd737068f97994063260207922403
hejj <junjihe@gmail.com>
2018-10-11 11:18:58 +0800

    init

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,109 +1,109 @@
-
 function relation(data, canvas) {
-  var a = data.node, b = data.link, force, d = canvas, e, f = 'id', g = 12, h = 800, i = 600, charge = -50, k = 80, l = 2;
+  var a = data.node, b = data.link, force, d = canvas, e, f = 'id', g = 12, h = 800, i = 600,
+    l = 2;
   this.init = function () {
     force = d3.forceSimulation()
-      .force('charge', d3.forceManyBody().strength(charge))
+      .force('charge', d3.forceManyBody().strength(-50))
       .force('center', d3.forceCenter(h / 2, i / 2))
       .force('collide', d3.forceCollide(1.2 * g)),
       force.nodes(a).on('tick', this.render),
       force.force('link', d3.forceLink().links(b)
         .id(function (a) {
-        return a[f]
-      }).distance(k)),
+          return a[f]
+        }).distance(80)),
       this.initDrag(),
       e = d.getContext('2d'),
       this.restart(),
       this.pause()
-  }
-    ,
-    this.render = function () {
-      e.clearRect(0, 0, h, i),
-        e.lineWidth = l,
-        e.strokeStyle = 'black',
-        e.beginPath(),
-        b.forEach(function (a) {
-          e.moveTo(a.source.x, a.source.y),
-            e.lineTo(a.target.x, a.target.y)
-        }),
-        e.stroke(),
-        e.lineWidth = 1,
-        e.strokeStyle = 'black',
-        a.forEach(function (a) {
-          e.fillStyle = '#A142FF',
-            e.beginPath(),
-            e.arc(a.x, a.y, a.radius, 0, 2 * Math.PI),
-            e.fill(),
-            e.stroke()
-        }),
-        e.font = '12px',
-        e.fillStyle = 'black',
-        e.textAlign = 'center',
-        a.forEach(function (a) {
-          e.fillText(a.name, a.x, a.y + 2.5 * a.radius)
-        })
+  };
+
+  this.render = function () {
+    e.clearRect(0, 0, h, i);
+      e.lineWidth = l;
+      e.strokeStyle = 'black';
+      e.beginPath();
+      b.forEach(function (a) {
+        e.moveTo(a.source.x, a.source.y);
+        e.lineTo(a.target.x, a.target.y);
+      });
+      e.stroke();
+      e.lineWidth = 1;
+      e.strokeStyle = 'black';
+      a.forEach(function (a) {
+        e.fillStyle = '#A142FF',
+          e.beginPath(),
+          e.arc(a.x, a.y, a.radius||10, 0, 2 * Math.PI),
+          e.fill(),
+          e.stroke()
+      }),
+      e.font = '12px',
+      e.fillStyle = 'black',
+      e.textAlign = 'center',
+      a.forEach(function (a) {
+        e.fillText(a.name, a.x, a.y + 2.5 * a.radius)
+      })
+  };
+
+  this.initDrag = function () {
+    function a() {
+      return force.find(d3.event.x, d3.event.y)
     }
-    ,
-    this.initDrag = function () {
-      function a() {
-        return force.find(d3.event.x, d3.event.y)
-      }
 
-      function b() {
-        d3.event.active || force.alphaTarget(.3).restart(),
-          d3.event.subject.fx = d3.event.subject.x,
-          d3.event.subject.fy = d3.event.subject.y
-      }
-
-      function e() {
-        d3.event.subject.fx = d3.event.x,
-          d3.event.subject.fy = d3.event.y
-      }
-
-      function f() {
-        d3.event.active || force.alphaTarget(0),
-          d3.event.subject.fx = null,
-          d3.event.subject.fy = null
-      }
-
-      d3.select(d).call(d3.drag().container(d).subject(a).on('start', b).on('drag', e).on('end', f))
+    function start() {
+      d3.event.active || force.alphaTarget(.3).restart();
+      d3.event.subject.fx = d3.event.subject.x;
+      d3.event.subject.fy = d3.event.subject.y;
     }
-    ,
-    this.pause = function () {
-      force.stop()
+
+    function drag() {
+      d3.event.subject.fx = d3.event.x;
+      d3.event.subject.fy = d3.event.y;
     }
-    ,
-    this.run = function () {
-      force.restart()
+
+    function end() {
+      d3.event.active || force.alphaTarget(0),
+        d3.event.subject.fx = null,
+        d3.event.subject.fy = null
     }
-    ,
-    this.restart = function () {
-      force.alpha(1),
-        this.run()
-    }
-}
+    d3.select(d).call(d3.drag().container(d).subject(a).on('start', start).on('drag', drag).on('end', end))
+  };
 
+  this.pause = function () {
+    force.stop()
+  };
 
+  this.run = function () {
+    force.restart()
+  };
 
+  this.restart = function () {
+    force.alpha(1),
+      this.run()
+  };
 
-function initRadius(r) {
-  r[0].radius = 18,
-    r[1].radius = 14,
-    r[2].radius = 14,
-    r[3].radius = 14,
-    r[4].radius = 14;
-  for (var o = 5; o < r.length; o++)
-    r[o].radius = 10
+  this.init();
 }
 
 function initRelationShip() {
 
-  var canvas = document.querySelector('canvas')
-    , width = 1300
-    , height = 600;
-  var data = {node:[{'id':'0','name':'Geoff'},{'id':'1','name':'School'},{'id':'2','name':'Company'},{'id':'3','name':'Family'},
-      {'id':'4','name':'Network'},{'id':'5','name':'Aaron'},{'id':'6','name':'Abbott'},{'id':'7','name':'Jacob'},{'id':'8','name':'James'},
-      {'id':'9','name':'Kelvin'},{'id':'10','name':'Kendall'},{'id':'11','name':'Kendrickw'},{'id':'12','name':'Leroy'},{'id':'13','name':'Leslie'},
+  var canvas = document.querySelector('canvas');
+  var data = {
+    node: [
+      {'id': '0', 'name': 'Geoff'},
+      {'id': '1', 'name': 'School'},
+      {'id': '2', 'name': 'Company'},
+      {
+      'id': '3',
+      'name': 'Family'
+    },
+      {'id': '4', 'name': 'Network'}, {'id': '5', 'name': 'Aaron'}, {'id': '6', 'name': 'Abbott'}, {
+        'id': '7',
+        'name': 'Jacob'
+      }, {'id': '8', 'name': 'James'},
+      {'id': '9', 'name': 'Kelvin'}, {'id': '10', 'name': 'Kendall'}, {'id': '11', 'name': 'Kendrickw'}, {
+        'id': '12',
+        'name': 'Leroy'
+      }, {'id': '13', 'name': 'Leslie'},
       {'id': '14', 'name': 'Lester'}, {'id': '15', 'name': 'Magnus'}, {'id': '16', 'name': 'Malcolm'}, {
         'id': '17',
         'name': 'Melvin'
@@ -165,17 +165,18 @@ function initRelationShip() {
       'target': 44
     }, {'source': 2, 'target': 45}, {'source': 3, 'target': 46}, {'source': 4, 'target': 47},
       {
-      'source': 1,
-      'target': 48
-    }, {
+        'source': 1,
+        'target': 48
+      }, {
         'source': 48,
         'target': 49
       }]
   };
-    initRadius(data.node);
+  // initRadius(data.node);
   var _relation = new relation(data, canvas);
-    _relation.init(),
-    _relation.run()
+  // _relation.init();
+    _relation.run();
 }
+
 initRelationShip();
 
